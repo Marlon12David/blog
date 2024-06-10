f69883355527820afdbce39ea5bcb44e176a9fcb
Marlon Abreo <moraabreo@gmail.com>
2024-06-10 18:51:54 -0500

    Creación factories para category, image, post, tag, creación seeder para post y user

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Marlon David',
+            'email' => 'marlon@gmail.com',
+            'password' => bcrypt('marlon123')
+        ]);
+
+        User::factory(99)->create();
+    }
+}
