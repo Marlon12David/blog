2da0315f378edc0e6b48e133bc1f5e94eca0df3e
Marlon Abreo <moraabreo@gmail.com>
2024-06-12 11:09:27 -0500

    Creación ruta, modificación controlador post y vista para mostrar cada post y posts similares según categoría

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,16 @@ class PostController extends Controller
         
         return view('posts.index', ['posts' => $posts]);
     }
+
+    public function show(Post $post){
+        
+        $similares = Post::where('category_id', $post->category_id)
+                            ->where('status', 2)
+                            ->where('id', '!=', $post->id)
+                            ->latest('id')
+                            ->take(4)
+                            ->get();
+
+        return view('posts.show', ['post' => $post, 'similares' => $similares]);
+    }
 }
