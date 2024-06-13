5c7e433d4a501f793d596ac629cb1d6e67c906f8
Marlon Abreo <moraabreo@gmail.com>
2024-06-13 10:27:53 -0500

    Creación ruta, modificación controlador post y vista para mostrar posts dependiendo de su etiqueta

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -34,5 +35,12 @@ class PostController extends Controller
                         ->latest('id')
                         ->paginate(6);
         return view('posts.category', ['posts' => $posts, 'category' => $category]);
+    }
+
+    public function tag(Tag $tag){
+
+        $posts = $tag->posts()->where('status', 2)->latest('id')->paginate(4);
+    
+        return view('posts.tag', ['posts' => $posts, 'tag' => $tag]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 Route::get('category/{category:slug}', [PostController::class, 'category'])->name('posts.category');
-
+Route::get('tag/{tag:slug}', [PostController::class, 'tag'])->name('posts.tag');
 
 
 Route::middleware([
